8ca530bd1aeaebae880124ce9ad9a2f9a1cc2d43
Akiho Kawada <kawadatakeshiho@gmail.com>
2024-01-21 18:33:28 +0900

    refac: 汎用化

--- 3A/multi-agent_system/report1/pso.py
+++ 3A/multi-agent_system/report1/pso.py
@@ -1,64 +1,72 @@
 import random
+from typing import List
 
 import numpy as np
 
 
-def update_position(x, y, vx, vy):
-    updated_x = x + vx
-    updated_y = y + vy
-    return updated_x, updated_y
+def update_position(x: np.ndarray, v: np.ndarray) -> np.ndarray:
+    updated_x = x + v
+    return updated_x
 
 
-def update_velosity(x, y, vx, vy, xp, xg, yp, yg, w=0.5, c1=0.14, c2=0.14):
+def update_velosity(
+    x: np.ndarray,
+    v: np.ndarray,
+    personal_best_position: np.ndarray,
+    global_best_position: np.ndarray,
+    w=0.5,
+    c1=0.14,
+    c2=0.14,
+) -> np.ndarray:
     r1 = random.uniform(0, 1)
     r2 = random.uniform(0, 1)
-    updated_vx = w * vx + c1 * r1(xp - x) + c2 * r2 * (xg - x)
-    updated_vy = w * vy + c1 * r1(yp - y) + c2 * r2 * (yg - y)
-    return updated_vx, updated_vy
+    updated_v = (
+        w * v
+        + c1 * r1(personal_best_position - x)
+        + c2 * r2 * (global_best_position - x)
+    )
+    return updated_v
 
 
-def main():
-    N = 100
-    x_min, x_max = -5, 5
-    y_min, y_max = -5, 5
-    # 粒子位置, 速度, パーソナルベスト, グローバルベストの初期化を行う
-    ps = [
-        {"x": random.uniform(x_min, x_max), "y": random.uniform(y_min, y_max)}
-        for i in range(N)
-    ]
-    vs = [{"x": 0.0, "y": 0.0} for i in range(N)]
-    personal_best_positions = list(ps)
-    personal_best_scores = [criterion(p["x"], p["y"]) for p in ps]
-    best_particle = np.argmin(personal_best_positions)
-    global_best_position = personal_best_positions[best_particle]
+def main(
+    num: int,
+    step_num: int,
+    initial: List[np.ndarray],  # 各要素にその点の全次元位置情報が含まれる
+    criterion,
+):
+    # initialize
+    assert len(initial) == num
+    position_list = initial
+    velosity_list = np.zeros(initial.shape)
+    personal_best_position_list = list(position_list)
+    personal_best_score_list = [criterion(position_list) for p in position_list]
+    best_particle_idx = np.argmin(personal_best_score_list)
+    global_best_position = personal_best_position_list[best_particle_idx]
 
-    T = 30
-    for t in range(T):
-        for n in range(N):
-            x, y = ps[n]["x"], ps[n]["y"]
-            vx, vy = vs[n]["x"], vs[n]["y"]
-            p = personal_best_positions[n]
+    for t in range(step_num):
+        for i in range(num):
+            x = position_list[i]
+            v = velosity_list[i]
+            personal_best_position = personal_best_position_list[i]
 
-            updated_x, updated_y = update_position(x, y, vx, vy)
-            ps[n] = {"x": updated_x, "y": updated_y}
+            updated_x = update_position(x, v)
+            position_list[i] = updated_x
 
-            xg = global_best_position["x"]
-            yg = global_best_position["y"]
-            xp = personal_best_positions["x"]
-            yp = personal_best_positions["y"]
-            updated_vx, updated_vy = update_velosity(x, y, vx, vy, xp, xg, yp, yg)
-            vs = {"x": updated_vx, "y": updated_vy}
+            updated_v = update_velosity(
+                x, v, personal_best_position, global_best_position
+            )
+            velosity_list[i] = updated_v
 
-            score = criterion(updated_x, updated_y)
-            if score < personal_best_scores[n]:
-                personal_best_scores[n] = score
-                personal_best_positions[n] = {"x": updated_x, "y": updated_y}
+            score = criterion(updated_x)
+            if score < personal_best_score_list[i]:
+                personal_best_score_list[i] = score
+                personal_best_position_list[i] = updated_x
 
-        best_particle = np.argmin(personal_best_scores)
-        global_best_position = personal_best_positions[best_particle]
+        best_particle_idx = np.argmin(personal_best_score_list)
+        global_best_position = personal_best_position_list[best_particle_idx]
 
         print(global_best_position)
-        print(min(personal_best_scores))
+        print(min(personal_best_score_list))
 
 
 if __name__ == "__main__":
