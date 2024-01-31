03dd29c8850cb2b805854fb2062bfd8f73649ece
Akiho Kawada <kawadatakeshiho@gmail.com>
2024-01-31 15:33:44 +0900

    chore: ファイル移動

--- 3A/multi-agent_system/report2/q_laerning.py
+++ 3A/multi-agent_system/report2/q_laerning.py
@@ -4,11 +4,14 @@ N = 10
 n = 3
 Q_list = np.zeros((N, N))
 
+
 def reward(target_pos, hunter_pos):
     pass
 
+
 def update_target_pos(target_pos):
     pass
+
 
 def update_hunter_pos(hunter_pos):
     pass
